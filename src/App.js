9bba102a619a0935cc0c077f6c01c641c317c095
Cameron Bailey <cameron.bailey@hoodgroup.co.uk>
2019-07-30 08:13:43 +0100

    Added prettier as a dev dependency and a prettier config file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.scss";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Skills from "./components/Skills/Skills";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Contact from "./components/Contact/Contact";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.scss'
+import Sidebar from './components/Sidebar/Sidebar'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
+import Portfolio from './components/Portfolio/Portfolio'
+import Contact from './components/Contact/Contact'
 
 function App() {
-  return (
-    <Router>
-      <div className="container">
-        <Sidebar />
+    return (
+        <Router>
+            <div className="container">
+                <Sidebar />
 
-        <div className="mainContent-container">
-          {/* Routes */}
-          <Route path="/" exact component={Home} />
-          <Route path="/about/" component={About} />
-          <Route path="/skills/" component={Skills} />
-          <Route path="/portfolio/" component={Portfolio} />
-          <Route path="/contact/" component={Contact} />
-        </div>
-      </div>
-    </Router>
-  );
+                <div className="mainContent-container">
+                    {/* Routes */}
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about/" component={About} />
+                    <Route path="/skills/" component={Skills} />
+                    <Route path="/portfolio/" component={Portfolio} />
+                    <Route path="/contact/" component={Contact} />
+                </div>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
