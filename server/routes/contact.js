8ba451332f6f3226e39956094bcb88d51a5c7f28
Cameron Bailey <cameron.bailey@hoodgroup.co.uk>
2019-08-27 11:22:39 +0100

    added .env file and dotenv npm package for handling environment variables

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
-
+const dotenv = require('dotenv').config();
 const emailHelper = require('../utils/emailHelper');
 
+// dotenv.config();
 const router = express.Router();
 
 router.post('/email', (req, res, next) => {
@@ -12,22 +13,20 @@ router.post('/email', (req, res, next) => {
     const transporter = nodemailer.createTransport(
         sendgridTransport({
             auth: {
-                api_key: emailHelper.emailApiKey
+                api_key: process.env.EMAIL_API_KEY
             }
         })
     );
-
-    transporter
-        .sendMail({
-            to: emailHelper.emailrecipient,
-            from: 'portfoilo@dev.com',
-            subject: req.body.subject,
-            html: '<h1>successfully signed up</h1>'
-        })
-        .catch(err => {
-            console.log(err);
-            return res.send(err);
-        });
+    transporter.sendMail({
+        to: process.env.EMAIL_RECIPIENT,
+        from: 'portfoilo@dev.com',
+        subject: req.body.subject,
+        html: '<h1>successfully signed up</h1>'
+    });
+    // .catch(err => {
+    //     console.log(err);
+    //     return res.send(err);
+    // });
 
     res.send('success');
 });
