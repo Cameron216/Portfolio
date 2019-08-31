f8f3443867e8dfa994649d710e9f291ad4133ed3
Cameron Bailey <CameronBailey216@hotmail.com>
2019-08-31 20:54:09 +0100

    Updated app.js to server index.html file from build folder in client

--- server/app.js
+++ server/app.js
@@ -15,6 +15,14 @@ app.use(bodyParser.json());
 
 app.use(contactRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    // app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.use('*', (req, res) => {
+        res.sendfile(path.join((__dirname = 'client/build/index.html')));
+    });
+}
+
 app.use((req, res, next) => {
     res.status(404).send('Page not found!');
 });
