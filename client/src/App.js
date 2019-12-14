8db1325200adab417b44745fdd543f8c8f571135
Cameron Bailey <CameronBailey216@hotmail.com>
2019-12-14 20:44:53 +0000

    Added react-helmet for adding meta tags for og and twitter cards.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.scss'
-import Sidebar from './components/Sidebar/Sidebar'
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import Skills from './components/Skills/Skills'
-import Portfolio from './components/Portfolio/Portfolio'
-import Contact from './components/Contact/Contact'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+import './App.scss';
+import Sidebar from './components/Sidebar/Sidebar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+
+import ogImg from './assets/images/og-card.jpg';
 
 function App() {
     return (
         <Router>
+            <Helmet>
+                <meta name="twitter:card" content="summary" />
+                <meta
+                    name="twitter:title"
+                    content="Cameron Bailey Web Developer"
+                />
+                <meta name="twitter:description" content="Online portfolio" />
+                <meta name="twitter:image" content={ogImg} />
+            </Helmet>
+
             <div className="container">
                 <Sidebar />
 
@@ -24,7 +38,7 @@ function App() {
                 </div>
             </div>
         </Router>
-    )
+    );
 }
 
-export default App
+export default App;
