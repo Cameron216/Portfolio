f941112bf11a761e837b02b323674873819bb170
Cameron Bailey <CameronBailey216@hotmail.com>
2019-12-14 21:20:15 +0000

    Testing og and twitter card.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+// import { Helmet } from 'react-helmet';
 
 import './App.scss';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -10,22 +10,11 @@ import Skills from './components/Skills/Skills';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 
-import ogImg from './assets/images/og-card.jpg';
+// import ogImg from './assets/images/og-card.jpg';
 
 function App() {
     return (
         <Router>
-            <Helmet>
-                <meta name="twitter:title" content="Cameron Bailey - Web Developer" />
-                <meta name="twitter:description" content=" Web Developer portfolio"/>
-                <meta name="twitter:image" content="https://pbs.twimg.com/media/DeZKBWEWsAA9Rng.jpg" />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta property="og:title" content="Cameron Bailey - Web Developer"/>
-                <meta property="og:description" content="Web Developer portfolio"/>
-                <meta property="og:image" content="https://pbs.twimg.com/media/DeZKBWEWsAA9Rng.jpg" />
-                <meta property="og:url" content="http://www.cameronbailey.co.uk" />
-            </Helmet>
-
             <div className="container">
                 <Sidebar />
 
